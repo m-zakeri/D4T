97e56fa05e05caab10dc98fa61206a6f1f6569ba
sadeghjafari5528 <sadeghjafari5528@gmail.com>
2022-02-18 13:05:34 +0330

    complete complexity

--- complexity.py
+++ complexity.py
@@ -9,11 +9,49 @@ class Complexity():
         self.inheritance_complexity_dic = {}
 
     def calculate_complexity(self):
+        # calculate hierarchical inheritance complexity
         inheritance_complexity_dic = {}
         candidate_nodes = self.__find_inheritance_candidates()
         for node in candidate_nodes:
             inheritance_complexity_dic[node] = self.__calculate_inheritance_complexity(node)
         print(inheritance_complexity_dic)
+
+        # calculate complexity between each pair of node
+        complexity_matrix = []
+        node_list = list(self.CDG.nodes)
+        node_list.sort()
+        print(node_list)
+        no_nodes = len(node_list)
+        for i_source in range(no_nodes):
+            complexity_matrix.append([])
+            for i_target in range(no_nodes):
+                complexity = self.__calculate_interaction_complexity(node_list[i_source],
+                                                                     node_list[i_target],
+                                                                     inheritance_complexity_dic)
+                complexity_matrix[i_source].append(complexity)
+                print((node_list[i_source], node_list[i_target]), complexity)
+                print('--------------')
+
+        #for i in complexity_matrix:
+        #    print(i)
+
+
+
+    def __calculate_interaction_complexity(self, source, target, inheritance_complexity_dic):
+        complexity = 1
+        print(source, target)
+        for path in nx.all_simple_paths(self.CDG, source=source, target=target):
+            complexity *= self.__calculate_path_complexity(path, inheritance_complexity_dic)
+            print('\t' ,path, complexity)
+        return complexity
+
+    def __calculate_path_complexity(self, path, inheritance_complexity_dic):
+        complexity = 1
+        for i in range(len(path) - 1):
+            if self.CDG[path[i]][path[i+1]]['relation_type'] == 'use_def':
+                if path[i] in inheritance_complexity_dic:
+                    complexity *= inheritance_complexity_dic[path[i]]
+        return complexity
 
     def __calculate_inheritance_complexity(self, node):
         complexity = 0
@@ -33,8 +71,6 @@ class Complexity():
             if is_leave:
                 complexity += depth_dic[current_node] * (depth_dic[current_node] - 1)
         return complexity
-
-
 
     def __find_inheritance_candidates(self):
         candidates = set()
