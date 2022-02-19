5898873ecedfa378c66d9ee31688ba39626d131b
sadeghjafari5528 <sadeghjafari5528@gmail.com>
2022-02-19 09:08:43 +0330

    prepare an API for complexity between 2 node

--- complexity.py
+++ complexity.py
@@ -6,51 +6,29 @@ import networkx as nx
 class Complexity():
     def __init__(self, CDG):
         self.CDG = CDG
-        self.inheritance_complexity_dic = {}
 
-    def calculate_complexity(self):
         # calculate hierarchical inheritance complexity
-        inheritance_complexity_dic = {}
+        self.inheritance_complexity_dic = {}
         candidate_nodes = self.__find_inheritance_candidates()
         for node in candidate_nodes:
-            inheritance_complexity_dic[node] = self.__calculate_inheritance_complexity(node)
-        print(inheritance_complexity_dic)
+            self.inheritance_complexity_dic[node] = self.__calculate_inheritance_complexity(node)
 
-        # calculate complexity between each pair of node
-        complexity_matrix = []
-        node_list = list(self.CDG.nodes)
-        node_list.sort()
-        print(node_list)
-        no_nodes = len(node_list)
-        for i_source in range(no_nodes):
-            complexity_matrix.append([])
-            for i_target in range(no_nodes):
-                complexity = self.__calculate_interaction_complexity(node_list[i_source],
-                                                                     node_list[i_target],
-                                                                     inheritance_complexity_dic)
-                complexity_matrix[i_source].append(complexity)
-                print((node_list[i_source], node_list[i_target]), complexity)
-                print('--------------')
-
-        #for i in complexity_matrix:
-        #    print(i)
-
-
-
-    def __calculate_interaction_complexity(self, source, target, inheritance_complexity_dic):
+    def calculate_interaction_complexity(self, source, target):
         complexity = 1
         print(source, target)
         for path in nx.all_simple_paths(self.CDG, source=source, target=target):
-            complexity *= self.__calculate_path_complexity(path, inheritance_complexity_dic)
-            print('\t' ,path, complexity)
+            complexity *= self.__calculate_path_complexity(path)
+            print('\t' ,path, self.__calculate_path_complexity(path))
+        print('\t' ,complexity)
+        print('-----------------------')
         return complexity
 
-    def __calculate_path_complexity(self, path, inheritance_complexity_dic):
+    def __calculate_path_complexity(self, path):
         complexity = 1
         for i in range(len(path) - 1):
             if self.CDG[path[i]][path[i+1]]['relation_type'] == 'use_def':
-                if path[i] in inheritance_complexity_dic:
-                    complexity *= inheritance_complexity_dic[path[i]]
+                if path[i] in self.inheritance_complexity_dic:
+                    complexity *= self.inheritance_complexity_dic[path[i]]
         return complexity
 
     def __calculate_inheritance_complexity(self, node):
@@ -88,5 +66,5 @@ if __name__ == "__main__":
     test_CDG = cd.get_CFG()
 
     c = Complexity(test_CDG)
-    c.calculate_complexity()
+    c.calculate_interaction_complexity(8,7)
 
