0ae4e200ad81ad431a811e5082eddc91f9180bc1
sadeghjafari5528 <sadeghjafari5528@gmail.com>
2022-05-09 09:31:56 +0430

    start refactoring of constructor injection case1

--- injection.py
+++ injection.py
@@ -280,6 +280,8 @@ class ConstructorEditorListener_v2(JavaParserLabeledListener):
         self.__method_depth = 0
         self.__class_depth = 0
         self.field_variables = dict()
+        self.dependees = dict()
+        self.constructors = list()
         self.index_dic = index_dic
         if common_token_stream is not None:
             self.token_stream_rewriter = TokenStreamRewriter(common_token_stream)
@@ -288,15 +290,75 @@ class ConstructorEditorListener_v2(JavaParserLabeledListener):
 
     def __get_field_variable_struct(self):
         field_variable = {
-            'modifiers':[],
+            'modifiers':list(),
             'identifier':None,
             'type':None,
             'declaration_location':None,
             'initiation_location':None,
-            'object_type':None
+            'initiation_place':None
         }
         return Struct(**field_variable)
 
+    def __get_dependee_struct(self):
+        dependee = {
+            'name': None,
+            'package': None,
+            'type': None
+        }
+        return Struct(**dependee)
+
+    def __get_constructor_struct(self):
+        constructor = {
+            'formal_parameters':list(),
+            'stop_location':None
+        }
+        return Struct(**constructor)
+
+    def __get_formal_parameter_struct(self):
+        formal_parameter = {
+            'identifier':None,
+            'type':None,
+            'location':None
+        }
+        return Struct(**formal_parameter)
+
+    def __check_constructor_injection_case1(self, variable):
+        pass
+
+    def __generate_constructor(self):
+        text = ''
+        formal_variable_text = ''
+        assign_text = ''
+        can_make_constructor = False
+        for v in self.field_variables:
+            v_info = self.field_variables[v]
+            if v_info['can_inject']:
+                can_make_constructor = True
+                ctx = v_info['ctx']
+                print('ctx:', ctx.getText())
+                # delete field variable instantiation
+                self.token_stream_rewriter.delete(program_name=self.token_stream_rewriter.DEFAULT_PROGRAM_NAME,
+                                                  from_idx=ctx.parentCtx.parentCtx.start.tokenIndex,
+                                                  to_idx=ctx.parentCtx.parentCtx.stop.tokenIndex + 1
+                                                  )
+                # print('@@@@@@@@@@@@@@@@@@@@@@')
+                # print(self.token_stream_rewriter.getDefaultText())
+
+                text += f"private {'I' + v_info['type']} {v};\n\t"
+                formal_variable_text += f"{'I' + v_info['type']} {v},"
+                assign_text += f"\n\t\tthis.{v} = {v};"
+        formal_variable_text = formal_variable_text[:-1]
+        assign_text = '{' + assign_text + '\n\t}'
+
+        text += f"public {self.currentClass} ({formal_variable_text})\n\t{assign_text}\n\n\t"
+
+        if can_make_constructor:
+            self.token_stream_rewriter.insertAfter(self.current_constructor_info['token_index'],
+                                                   text,
+                                                   program_name=self.token_stream_rewriter.DEFAULT_PROGRAM_NAME)
+
+    def __edit_constructors(self):
+        pass
 
     def enterPackageDeclaration(self, ctx: JavaParserLabeled.PackageDeclarationContext):
         pass
@@ -316,6 +378,10 @@ class ConstructorEditorListener_v2(JavaParserLabeledListener):
     def exitClassDeclaration(self, ctx: JavaParserLabeled.ClassDeclarationContext):
         self.__class_depth -= 1
         if self.__class_depth == 0:
+            if len(self.constructors) == 0: #only can case 1 happen
+                self.__generate_constructor()
+            else:
+                self.__edit_constructors()
             self.currentClass = None
 
     def enterClassBody(self, ctx: JavaParserLabeled.ClassBodyContext):
@@ -327,19 +393,100 @@ class ConstructorEditorListener_v2(JavaParserLabeledListener):
             classBodyDeclaration = ctx.parentCtx.parentCtx
             modifiers = []
             _type = ctx.typeType().getText()
-            declaration_location = (ctx.start.tokenIndex, ctx.stop.tokenIndex + 1)
+            if _type not in self.dependees:
+                dependee = self.__get_dependee_struct()
+                dependee.name = _type
+                dependee.package = Path.find_package_of_dependee(
+                    _type,
+                    self.imports,
+                    self.imports_star,
+                    self.index_dic
+                )
+                if dependee.package is not None:
+                    dependee.type = self.index_dic[f"{dependee.package}-{dependee.name}-{dependee.name}"]["type"]
+                    print(dependee.__dict__)
+                    self.dependees[_type] = dependee
+            start_declaration_location = ctx.start.tokenIndex
+
             for modifier in classBodyDeclaration.modifier():
                 modifiers.append(modifier.getText())
 
             for identifier in ctx.variableDeclarators().variableDeclarator():
+                stop_declaration_location = identifier.variableDeclaratorId().stop.tokenIndex
                 field_variable = self.__get_field_variable_struct()
                 field_variable.modifiers = modifiers
-                field_variable.identifier = identifier.getText()
+                field_variable.identifier = identifier.variableDeclaratorId().getText()
                 field_variable.type = _type
-                field_variable.declaration_location = declaration_location
-                print(field_variable.__dict__)
+                field_variable.declaration_location = (start_declaration_location, stop_declaration_location)
+                self.field_variables[identifier.variableDeclaratorId().getText()] = field_variable
 
-            #TODO: add initiate_location and object_type
+                # print(self.token_stream_rewriter.getText(
+                #     program_name=self.token_stream_rewriter.DEFAULT_PROGRAM_NAME,
+                #     start=start_declaration_location,
+                #     stop=stop_declaration_location
+                # ))
+                # print(field_variable.__dict__)
+
+    def enterVariableInitializer1(self, ctx:JavaParserLabeled.VariableInitializer1Context):
+        if self.__class_depth == 1:
+            if ctx.children[0].children[0].getText() == 'new':
+                for child in ctx.parentCtx.parentCtx.children:
+                    # print(dir(child))
+                    # print(type(child).__name__)
+                    if type(child).__name__ == 'VariableDeclaratorContext':
+                        if child.variableDeclaratorId().getText() in self.field_variables:
+                            self.field_variables[child.variableDeclaratorId().getText()].initiation_location = \
+                                (ctx.start.tokenIndex - 2, ctx.stop.tokenIndex)
+                            self.field_variables[child.variableDeclaratorId().getText()].initiation_place = 'fieldDeclaration'
+                            print('initializer:')
+                            # print(self.token_stream_rewriter.getText(
+                            #     program_name=self.token_stream_rewriter.DEFAULT_PROGRAM_NAME,
+                            #     start=ctx.start.tokenIndex - 2,
+                            #     stop=ctx.stop.tokenIndex
+                            # ))
+                            print(self.field_variables[child.variableDeclaratorId().getText()].__dict__)
+
+    def enterExpression4(self, ctx:JavaParserLabeled.Expression4Context):
+        if self.__class_depth == 1:
+            identifier_list = ctx.parentCtx.children[0].getText()
+            identifier_list = identifier_list.split('.')
+            if len(identifier_list) == 1:
+                identifier = identifier_list[0]
+            elif len(identifier_list) == 2 and identifier_list[0] == 'this':
+                identifier = identifier_list[1]
+            else:
+                identifier = None
+
+            if identifier is not None:
+                if identifier in self.field_variables:
+                    self.field_variables[identifier].initiation_location = \
+                        (ctx.start.tokenIndex, ctx.stop.tokenIndex)
+                    self.field_variables[identifier].initiation_place = 'constructor'
+                    print('initializer2:')
+                    # print(self.token_stream_rewriter.getText(
+                    #     program_name=self.token_stream_rewriter.DEFAULT_PROGRAM_NAME,
+                    #     start=ctx.start.tokenIndex,
+                    #     stop=ctx.stop.tokenIndex
+                    # ))
+                    print(self.field_variables[identifier].__dict__)
+
+    def enterConstructorDeclaration(self, ctx: JavaParserLabeled.ConstructorDeclarationContext):
+        if self.__class_depth == 1:
+            constructor = self.__get_constructor_struct()
+            constructor.stop_location = ctx.stop.tokenIndex
+            for formal_parameter in ctx.formalParameters().formalParameterList().formalParameter():
+                formal_parameter_s = self.__get_formal_parameter_struct()
+                formal_parameter_s.identifier = formal_parameter.variableDeclaratorId().getText()
+                formal_parameter_s.type = formal_parameter.typeType().getText()
+                formal_parameter_s.location = (
+                    formal_parameter.start.tokenIndex,
+                    formal_parameter.stop.tokenIndex
+                )
+                print(formal_parameter_s.__dict__)
+                constructor.formal_parameters.append(formal_parameter_s)
+            print(constructor.__dict__)
+
+
 
     def enterVariableDeclarator(self, ctx: JavaParserLabeled.VariableDeclaratorContext):
         if self.__class_depth == 1:
@@ -350,10 +497,6 @@ class ConstructorEditorListener_v2(JavaParserLabeledListener):
             pass
 
     def enterFormalParameter(self, ctx: JavaParserLabeled.FormalParameterContext):
-        if self.__class_depth == 1:
-            pass
-
-    def enterConstructorDeclaration(self, ctx: JavaParserLabeled.ConstructorDeclarationContext):
         if self.__class_depth == 1:
             pass
 
