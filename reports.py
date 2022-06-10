cc39894faffd6e8b8b5f41deb7bc615c441849a4
sadeghjafari5528 <sadeghjafari5528@gmail.com>
2022-06-10 10:38:27 +0430

    compelete report of factory_pandas

--- reports.py
+++ reports.py
@@ -1,45 +1,79 @@
 import json
+import pandas as pd
 
 from class_diagram import ClassDiagram
 from factory import Factory
 from utils import File
 import config
-import networkx as nx
-import pandas as pd
+
 
 class Report:
-    def __init__(self):
-        pass
+    def __init__(self, java_project):
+        self.java_project = java_project
+        self.java_project_address = config.projects_info[java_project]['path']
+        self.base_dirs = config.projects_info[java_project]['base_dirs']
+        self.files = File.find_all_file(self.java_project_address, 'java')
+        self.index_dic = File.indexing_files_directory(self.files, 'class_index.json', self.base_dirs)
+        self.cd = ClassDiagram(self.java_project_address, self.base_dirs, self.index_dic)
+        self.cd.make_class_diagram()
 
-    @staticmethod
-    def get_factory_report(java_project, sensitivity):
+    def reload_from_disk(self):
+        self.java_project_address = config.projects_info[self.java_project]['path']
+        self.base_dirs = config.projects_info[self.java_project]['base_dirs']
+        self.files = File.find_all_file(self.java_project_address, 'java')
+        self.index_dic = File.indexing_files_directory(self.files, 'class_index.json', self.base_dirs)
+        self.cd = ClassDiagram(self.java_project_address, self.base_dirs, self.index_dic)
+        self.cd.make_class_diagram()
+
+    def get_json_factory_report(self, sensitivity, save=True, edit=True):
         report = {
             "java_project":java_project,
             "sensitivity":sensitivity,
             "cases":None
         }
-        java_project_address = config.projects_info[java_project]['path']
-        base_dirs = config.projects_info[java_project]['base_dirs']
-        files = File.find_all_file(java_project_address, 'java')
-        index_dic = File.indexing_files_directory(files, 'class_index.json', base_dirs)
-        cd = ClassDiagram(java_project_address, base_dirs, index_dic)
-        cd.make_class_diagram()
 
         f = Factory()
-        report["cases"] = f.refactor(sensitivity, index_dic, cd.class_diagram_graph, base_dirs)
-        with open(java_project_address + "/factory_report.json", 'w') as f:
-            json.dump(report, f, indent=4)
+        report["cases"] = f.refactor(
+            sensitivity,
+            self.index_dic,
+            self.cd.class_diagram_graph,
+            self.base_dirs,
+            edit=edit
+        )
 
-        files = File.find_all_file(java_project_address, 'java')
-        index_dic = File.indexing_files_directory(files, 'class_index.json', base_dirs)
-        cd2 = ClassDiagram(java_project_address, base_dirs, index_dic)
-        cd2.make_class_diagram()
+        if save:
+            with open(self.java_project_address + "/factory_report.json", 'w') as f:
+                json.dump(report, f, indent=4)
+        return report
+
+    def get_pandas_factory_report(self, json_report, save=True):
+        pandas_report = {
+            "package": [],
+            "path": [],
+            "class": [],
+            "sensitivity": []
+        }
+
+        for case in json_report["cases"]:
+            factory = case["factory"]
+            pandas_report["package"].append(factory["package"])
+            pandas_report["path"].append(factory["path"])
+            pandas_report["class"].append(factory["class_name"])
+            pandas_report["sensitivity"].append(json_report["sensitivity"])
+
+        df = pd.DataFrame(pandas_report)
+
+        if save:
+            df.to_csv(self.java_project_address + "/factory_report.csv", index=False)
+        return df
+
+
+
 
 
 
 if __name__ == "__main__":
     java_project = "10_water-simulator"
-    Report.get_factory_report(
-        java_project,
-        0.1
-    )
+    r = Report(java_project)
+    json_report = r.get_json_factory_report(0.1, edit=False)
+    pandas_report = r.get_pandas_factory_report(json_report)
