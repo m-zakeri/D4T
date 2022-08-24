c2271e97c9b28f72c6583619dade0997fb14004b
صادق جعفری <sadeghjafari5079@gmail.com>
2022-08-24 16:41:57 +0430

    change main

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
         base_dirs = config.projects_info[java_project]['base_dirs']
         files = File.find_all_file(java_project_address, 'java')
         index_dic = File.indexing_files_directory(files, 'class_index.json', base_dirs)
-        cd = ClassDiagram(java_project_address, base_dirs, index_dic)
+        cd = ClassDiagram(java_project_address, base_dirs, files, index_dic)
         cd.make_class_diagram()
         #cd.save('class_diagram.gml')
         #cd.load('class_diagram.gml')
@@ -35,9 +35,11 @@ if __name__ == "__main__":
 
         files = File.find_all_file(java_project_address, 'java')
         index_dic = File.indexing_files_directory(files, 'class_index.json', base_dirs)
-        cd2 = ClassDiagram(java_project_address, base_dirs, index_dic)
+        cd2 = ClassDiagram(java_project_address, base_dirs, files, index_dic)
         cd2.make_class_diagram()
         cd2.show(cd2.class_diagram_graph)
+        CDG = cd2.get_CDG()
+        cd.show(CDG)
         # g = cd2.class_diagram_graph
         # print(len(list(nx.weakly_connected_components(g))))
         # for i in nx.weakly_connected_components(g):
